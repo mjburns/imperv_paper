ca68f6ec0b7c4f66cdd376af8ba2cdfb1a3af303
Matthew Burns <burnsm@rdl-t86dyl.desktop.cloud.unimelb.edu.au>
2024-08-22 04:20:36 +0000

    Landuse information for lsc sites.

--- Prepare_LSC_data.R
+++ Prepare_LSC_data.R
@@ -41,8 +41,79 @@ data <- data.df.tibble %>%
 
 ## Save the lsc events
 save(data, file = paste(here::here(""), "/Input/lsc_events_no_precip_max.Rdata", sep = ""))
-       
-     
+
+## Get the LSC land-use data
+landuse <- get(load("~/Documents/imperv_paper/Input/ei_ts_11_sites_correct.rda"))
+landuse$lubri_date <- ymd(landuse$date, tz = "UTC")
+
+#levels(factor(landuse$sitecode))
+
+## Screen landuse to only get the sites of interest for the impervious paper
+## Get lower and upper bound based on date_start and date_end
+landuse_screened <- landuse[(!landuse$sitecode %in% (c("BRS0015", "DBS0008", "FER0006", "LYR0007", "OLN0009", "SAS0002"))),]
+
+## Floor date to day
+date_start_day <- floor_date(date_start, "day")
+date_end_day <- floor_date(date_end, "day")
+
+## Declare null variable
+landuse_postscm <- NULL
+
+for(i in 1:NROW(levels(factor(landuse_screened$sitecode)))) {
+  
+  get_site_i_data <- landuse[which(landuse_screened$sitecode == levels(factor(landuse_screened$sitecode))[i]),]
+  
+  ## Screen by dates
+  get_site_i_post_scm_early <- get_site_i_data[which(get_site_i_data$lubri_date == date_start_day),]
+  get_site_i_post_scm_later <- get_site_i_data[which(get_site_i_data$lubri_date == date_end_day),]
+
+  ## Build a data.frame for the sites
+  landuse_i_df <- data.frame(sitecode = get_site_i_post_scm_early$sitecode[1], ti_early = get_site_i_post_scm_early$ti[1], ti_later = get_site_i_post_scm_later$ti[1],
+                             ei_early = get_site_i_post_scm_early$ei[1], ei_later = get_site_i_post_scm_later$ei[1], s_early = get_site_i_post_scm_early$s[1],
+                             s_later = get_site_i_post_scm_later$s[1])
+  
+  #Combine into the above
+  landuse_postscm <- rbind(landuse_postscm, landuse_i_df) } #End the loop now
+
+
+## Get the same site codes as the paper
+## "Br" "D4" "Fe" "L1" "L4" "Ln" "Ls" "Ly" "Ol" "Sa"
+
+sites_match <- data.frame(sitecode = c("LIS0001", "LSN0001", "LSS0001", "LIS0004", "DBS0004"), shorter_name = c("L1", "Ln", "Ls", "L4", "D4"))
+
+
+## Use a match
+landuse_postscm$imperv_sitecode <-  sites_match$shorter_name[match(landuse_postscm$sitecode, sites_match$sitecode)]
+
+
+plot(c(1,1), c(landuse_postscm$ti_early[1], landuse_postscm$ti_later[1]), type = 'o', ylim = c(0,0.3), axes = FALSE, xlim = c(-1,6), xlab = "Site", ylab = "Metric")
+lines(c(1.1,1.1), c(landuse_postscm$ei_early[1], landuse_postscm$ei_later[1]), col = 'red', type = 'o')
+lines(c(1.2,1.2), c(landuse_postscm$s_early[1], landuse_postscm$s_later[1]), col = 'blue', type = 'o')
+
+lines(c(2.0,2.0), c(landuse_postscm$ei_early[2], landuse_postscm$ei_later[2]), col = 'black', type = 'o')
+lines(c(2.1,2.1), c(landuse_postscm$ei_early[2], landuse_postscm$ei_later[2]), col = 'red', type = 'o')
+lines(c(2.2,2.2), c(landuse_postscm$s_early[2], landuse_postscm$s_later[2]), col = 'blue', type = 'o')
+
+
+lines(c(3.0,3.0), c(landuse_postscm$ei_early[3], landuse_postscm$ei_later[3]), col = 'black', type = 'o')
+lines(c(3.1,3.1), c(landuse_postscm$ei_early[3], landuse_postscm$ei_later[3]), col = 'red', type = 'o')
+lines(c(3.2,3.2), c(landuse_postscm$s_early[3], landuse_postscm$s_later[3]), col = 'blue', type = 'o')
+
+
+lines(c(4.0,4.0), c(landuse_postscm$ei_early[4], landuse_postscm$ei_later[4]), col = 'black', type = 'o')
+lines(c(4.1,4.1), c(landuse_postscm$ei_early[4], landuse_postscm$ei_later[4]), col = 'red', type = 'o')
+lines(c(4.2,4.2), c(landuse_postscm$s_early[4], landuse_postscm$s_later[4]), col = 'blue', type = 'o')
+
+
+lines(c(5.0,5.0), c(landuse_postscm$ei_early[5], landuse_postscm$ei_later[5]), col = 'black', type = 'o')
+lines(c(5.1,5.1), c(landuse_postscm$ei_early[5], landuse_postscm$ei_later[5]), col = 'red', type = 'o')
+lines(c(5.2,5.2), c(landuse_postscm$s_early[5], landuse_postscm$s_later[5]), col = 'blue', type = 'o')
+
+
+axis(1, at = c(-1, 1, 2, 3, 4, 5, 6), lab = c("", landuse_postscm$imperv_sitecode, ""))
+axis(2)
+
+
 
 
 
